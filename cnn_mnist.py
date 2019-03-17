cee21d8c3f6e9e31d92b40d22f6a2a4cdccfd2ed
Emil Larsson <emil.larsson@eqtpartners.com>
2019-03-17 15:21:32 +0100

    Keras with TF 2.0

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -1,216 +1,136 @@
 """Cnn MNIST"""
-
 import os
-import tensorflow as tf
-
 from argparse import ArgumentParser
 
-tf.logging.set_verbosity(tf.logging.INFO)
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import tensorflow_datasets as tfds
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
-def cnn_model_fn(features, labels, mode, params):
-    input_layer = tf.reshape(features, [-1, 28, 28, 1])
-
-    conv1 = tf.layers.conv2d(
-        inputs=input_layer,
-        filters=6,
-        kernel_size=[6, 6],
-        strides=1,
-        padding='SAME',
-        activation=tf.nn.relu)
-
-    drop1 = tf.layers.dropout(
-        inputs=conv1, rate=0.25, training=mode == tf.estimator.ModeKeys.TRAIN)
-
-    conv2 = tf.layers.conv2d(
-        inputs=conv1,
-        filters=12,
-        kernel_size=[5, 5],
-        strides=2,
-        padding='SAME',
-        activation=tf.nn.relu)
-
-    drop2 = tf.layers.dropout(
-        inputs=conv2, rate=0.25, training=mode == tf.estimator.ModeKeys.TRAIN)
-
-    conv3 = tf.layers.conv2d(
-        inputs=drop2,
-        filters=24,
-        kernel_size=[4, 4],
-        strides=2,
-        padding='SAME',
-        activation=tf.nn.relu)
-
-    # Flatten tensor into a batch of vectors
-    # Input Tensor Shape: [batch_size, 7, 7, 24]
-    # Output Tensor Shape: [batch_size, 7 * 7 * 24]
-    conv3_flat = tf.reshape(conv3, [-1, 7 * 7 * 24])
-
-    # Dense Layer
-    # Densely connected layer with 200 neurons
-    # Input Tensor Shape: [batch_size, 7 * 7 * 24]
-    # Output Tensor Shape: [batch_size, 200]
-    dense = tf.layers.dense(inputs=conv3_flat, units=200, activation=tf.nn.relu)
-
-    # Add dropout operation; 0.25 probability that element will be kept
-    dropout = tf.layers.dropout(
-        inputs=dense, rate=params['dropout_rate'], training=mode == tf.estimator.ModeKeys.TRAIN)
-
-    # Logits layer
-    # Input Tensor Shape: [batch_size, 200]
-    # Output Tensor Shape: [batch_size, 10]
-    logits = tf.layers.dense(inputs=dropout, units=10)
-
-    tf.summary.histogram('conv1', conv1)
-    tf.summary.histogram('conv2', conv2)
-    tf.summary.histogram('conv3', conv3)
-    tf.summary.histogram('dense', dense)
-    tf.summary.histogram('logits', logits)
-
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        'classes': tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
-    }
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-    # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-    # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        # optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
-        optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    accuracy = tf.metrics.accuracy(
-        labels=labels, predictions=predictions['classes'])
-
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {'accuracy': accuracy}
-    tf.summary.scalar('accuracy', accuracy)  # Add accuracy to TRAIN also
-
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
-def data_input_fn(file_pattern, batch_size=1000, shuffle=False):
-    def _parser(record):
-        keys_to_features = {
-            'image_raw': tf.FixedLenFeature([], tf.string),
-            'label': tf.FixedLenFeature([], tf.int64)
-        }
-        parsed = tf.parse_single_example(record, keys_to_features)
-        label = tf.cast(parsed.pop('label'), dtype=tf.int32)
-
-        image = tf.decode_raw(parsed['image_raw'], tf.uint8)
-        image.set_shape((28 * 28))
-
-        # Normalize image
-        image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-
-        return image, label
-
-    files = tf.data.Dataset.list_files(file_pattern)
-    dataset = tf.data.TFRecordDataset(files, num_parallel_reads=32)
-
-    dataset = dataset.apply(
-        tf.contrib.data.map_and_batch(_parser, batch_size=batch_size, num_parallel_batches=4))
-
-    if shuffle:
-        dataset = dataset.apply(
-            tf.contrib.data.shuffle_and_repeat(buffer_size=10_000, count=None))
-    else:
-        dataset = dataset.repeat(count=1)
-
-    dataset = dataset.prefetch(2)
-    iterator = dataset.make_one_shot_iterator()
-    images, labels = iterator.get_next()
-
-    return images, labels
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        '--data-directory',
-        default='/tmp/data/mnist',
-        help='Directory where the data is stored'
+        "--model-dir",
+        default="/tmp/mnist_cnn",
+        help="Directory where model summaries and checkpoints are stored",
     )
     parser.add_argument(
-        '--model-directory',
-        default='/tmp/mnist_cnn',
-        help='Directory where model summaries and checkpoints are stored'
+        "--learning-rate", default=0.003, type=float, help="The learning rate"
     )
     parser.add_argument(
-        '--learning-rate',
-        default=0.003,
-        type=float,
-        help='The learning rate'
+        "--dropout-rate", default=0.15, type=float, help="The dropout rate"
     )
     parser.add_argument(
-        '--dropout-rate',
-        default=0.75,
-        type=float,
-        help='The dropout rate'
-    )
-    parser.add_argument(
-        '--train-batch-size',
+        "--train-batch-size",
         default=128,
         type=int,
-        help='Batch Size when training'
+        help="Batch Size when training",
+    )
+    parser.add_argument(
+        "--epochs", default=20, type=int, help="Batch Size when training"
     )
     args = parser.parse_args()
 
-    run_config = tf.estimator.RunConfig().replace(
-        save_summary_steps=600,
-        save_checkpoints_secs=600,
+    ds, info = tfds.load("mnist", with_info=True, as_supervised=True)
+    ds_train, ds_test = ds["train"], ds["test"]
+
+    def convert_types(image, label):
+        image = tf.cast(image, tf.float32)
+        image /= 255
+        return image, label
+
+    ds_train = (
+        ds_train.map(convert_types)
+        .shuffle(1000)
+        .batch(args.train_batch_size)
+        .prefetch(AUTOTUNE)
+    )
+    ds_test = ds_test.map(convert_types).batch(32)
+
+    model = models.Sequential(
+        [
+            layers.Conv2D(
+                32, (3, 3), activation="relu", input_shape=(28, 28, 1)
+            ),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.Flatten(),
+            layers.Dense(64, activation="relu"),
+            layers.Dropout(args.dropout_rate),
+            layers.Dense(10, activation="softmax"),
+        ]
     )
 
-    # Create the Estimator
-    estimator = tf.estimator.Estimator(
-        model_fn=cnn_model_fn,
-        config=run_config,
-        model_dir=args.model_directory,
-        params={
-            'learning_rate': args.learning_rate,
-            'dropout_rate': args.dropout_rate
-        }
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+
+    train_loss = tf.keras.metrics.Mean(name="train_loss")
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name="train_accuracy"
     )
 
-    # Set up logging for predictions
-    # Log the values in the "Softmax" tensor with label "probabilities"
-    tensors_to_log = {'probabilities': 'softmax_tensor'}
-    logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=50)
+    test_loss = tf.keras.metrics.Mean(name="test_loss")
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name="test_accuracy"
+    )
 
-    def train_input_fn():
-        return data_input_fn(
-            os.path.join(args.data_directory, 'train.tfrecords'),
-            batch_size=args.train_batch_size,
-            shuffle=True
+    log_dir = os.path.join(args.model_dir, "logs")
+    train_summary_writer = tf.summary.create_file_writer(
+        os.path.join(log_dir, "train")
+    )
+    test_summary_writer = tf.summary.create_file_writer(
+        os.path.join(log_dir, "test")
+    )
+
+    @tf.function
+    def train_step(image, label):
+        with tf.GradientTape() as tape:
+            predictions = model(image)
+            loss = loss_object(label, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(label, predictions)
+
+    @tf.function
+    def test_step(image, label):
+        predictions = model(image)
+        t_loss = loss_object(label, predictions)
+
+        test_loss(t_loss)
+        test_accuracy(label, predictions)
+
+    for epoch in range(args.epochs):
+        for image, label in ds_train:
+            train_step(image, label)
+        with train_summary_writer.as_default():
+            tf.summary.scalar("loss", train_loss.result(), step=epoch)
+            tf.summary.scalar("accuracy", train_accuracy.result(), step=epoch)
+
+        for test_image, test_label in ds_test:
+            test_step(test_image, test_label)
+        with test_summary_writer.as_default():
+            tf.summary.scalar("loss", test_loss.result(), step=epoch)
+            tf.summary.scalar("accuracy", test_accuracy.result(), step=epoch)
+
+        template = "Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}"
+        print(
+            template.format(
+                epoch + 1,
+                train_loss.result(),
+                train_accuracy.result() * 100,
+                test_loss.result(),
+                test_accuracy.result() * 100,
+            )
         )
 
-    def eval_input_fn():
-        return data_input_fn(
-            os.path.join(args.data_directory, 'test.tfrecords'),
-            batch_size=1000
-        )
-
-    # feature_spec = tf.feature_column.make_parse_example_spec(feature_columns)
-    # serving_input_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)
-
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=40000)
-    # export_latest = tf.estimator.LatestExporter(
-    #     name='serving', serving_input_receiver_fn=serving_input_fn)
-    eval_spec = tf.estimator.EvalSpec(
-        input_fn=eval_input_fn, throttle_secs=30)#, exporters=export_latest, steps=None)
-
-    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
-
+        # Reset metrics every epoch
+        train_loss.reset_states()
+        test_loss.reset_states()
+        train_accuracy.reset_states()
+        test_accuracy.reset_states()
