0d975bd89ac676d17b99d736a9096628c43d1712
Emil Larsson <emil.larsson@eqtpartners.com>
2019-03-17 15:53:17 +0100

    Add readme

--- show_tfrecord.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-
-for example in tf.python_io.tf_record_iterator("/tmp/data/mnist/train.tfrecords"):
-    result = tf.train.Example.FromString(example)
-    print(result)
-    break
